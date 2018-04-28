a833d53188736261f9e15c5427ce95dc5615a704
Smallwatermelon <38370858+Smallwatermelon@users.noreply.github.com>
2018-04-28 11:01:41 +0800

    Add files via upload

---
+++ githubc2/1.js
@@ -0,0 +1,6 @@
+function sort(type, order) {
+    var ids=document.getElementsByClassName('ids');
+    var price=document.getElementsByClassName('price');
+    var sales=document.getElementsByClassName('sales');
+    var all=new Object()
+}
